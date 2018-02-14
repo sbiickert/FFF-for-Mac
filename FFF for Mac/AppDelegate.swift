cc3628d8f7ac6cef1c120efb0079632c198a9bd8
Simon Biickert <simon@biickert.ca>
2018-02-13 19:50:20 -0700

    Initial Commit

---
+++ FFF for Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FFF for Mac
+//
+//  Created by Simon Biickert on 2018-02-13.
+//  Copyright © 2018 ii Softwerks. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ FFF for Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FFF for Mac
+//
+//  Created by Simon Biickert on 2018-02-13.
+//  Copyright © 2018 ii Softwerks. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
